02698bb3d0784f04ccc1a2050ef3cbc7d12ba333
dante7 <dante.arkade7@gmail.com>
2022-01-04 14:33:08 -0600

    Add binary test

--- lib/prime_types.ex
+++ lib/prime_types.ex
@@ -10,6 +10,11 @@ defmodule PrimeTypes do
   #     |> Enum.join("\n")
   #   )
   # end
+  
+  # SieveOfEratosthenes.calculate_primes(1_000_000)
+  # |> Enum.map(p, fn x -> Integer.to_string(x,2) end) 
+  # |> Enum.filter(fn x -> not String.contains?(x, "0") end) 
+  # |> Enum.map(fn x -> Integer.parse(x, 2) |> elem(0) end)
 
   def is_prime?(input) do
     primes = get_primes(input, 1)
@@ -28,9 +33,14 @@ defmodule PrimeTypes do
       :chen -> "Prime type not implemented"
       :circular -> "Prime type not implemented"
       :cousin -> PrimeTypes.Cousin.belonging_to?(prime_number)
+      :fermat -> PrimeTypes.Fermat.belonging_to?(prime_number)
       :twin -> PrimeTypes.Twin.belonging_to?(prime_number)
       _ -> "Prime type not implemented"
     end
+  end
+
+  def binary_primes do
+    
   end
 
 end
